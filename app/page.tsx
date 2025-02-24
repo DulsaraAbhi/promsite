3467c828effbc46b6d74e199f7e4f8dfcabf2f71
Dulsara Abhiman <158539197+DulsaraAbhi@users.noreply.github.com>
2025-02-24 22:06:52 +0530

    Fix footer

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,8 @@ export default function Home() {
           <span className="text-xl font-bold text-blue-600">LunaRest</span>
         </div>
         <nav className="flex space-x-6">
-          <a href="#features" className="text-black hover:text-black">
+        <a href="#features" className="text-black hover:text-black">
+
             Features
           </a>
           <a href="#team" className="text-black hover:text-black">
@@ -365,11 +366,11 @@ export default function Home() {
       <div className="mb-4">
         <div className="max-w-5xl mx-auto text-center">
         <p className="text-lg text-gray-600 ">
-          Email: <a href="mailto:your-email@example.com" className="text-black-600 hover:underline">lunarest2024@gmail.com</a>
+          Email: <a href="mailto:your-email@example.com" className="text-black-600 hover:underline text-center">lunarest2024@gmail.com</a>
         </p>
         <div className="max-w-5xl mx-auto text-center">
         <p className="text-lg text-gray-600 ">
-            Mobile: <a href="tel:+15551234567" className="text-black-600 hover:underline">+94000000000000</a>
+            Mobile: <a href="tel:+15551234567" className="text-black-600 hover:underline text-center">+94000000000000</a>
         </p>
       </div>
         <div className="max-w-5xl mx-auto text-center">
